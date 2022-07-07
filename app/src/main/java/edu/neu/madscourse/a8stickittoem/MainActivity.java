7866e4ea7edfcb069eac73a3bcc3f67b31973bb4
zack xue <zackxue@Zacks-MacBook-Pro-14inch.local>
2022-07-07 15:48:02 -0700

    first commit init git

---
+++ app/src/main/java/edu/neu/madscourse/a8stickittoem/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.neu.madscourse.a8stickittoem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
