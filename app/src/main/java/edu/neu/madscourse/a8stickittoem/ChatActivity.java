53439e9eaba843fbcb82b412279091bf0ac051fc
eddieyan95 <yufanyan95@gmail.com>
2022-07-10 19:22:42 -0400

    Add message activity, not completed

---
+++ app/src/main/java/edu/neu/madscourse/a8stickittoem/ChatActivity.java
@@ -0,0 +1,27 @@
+package edu.neu.madscourse.a8stickittoem;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.database.DatabaseReference;
+
+public class ChatActivity extends AppCompatActivity {
+    private FirebaseAnalytics mFirebaseAnalytics;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+
+
+
+    }
+}
---
+++ app/src/main/java/edu/neu/madscourse/a8stickittoem/Message.java
@@ -0,0 +1,46 @@
+package edu.neu.madscourse.a8stickittoem;
+
+import java.security.cert.CertPathBuilderResult;
+import java.util.Date;
+
+
+//create model to store the chat in the firebase real-time database
+public class Message{
+    private String user;
+    private long time;
+    private String stickerID;
+
+    public Message(String stickerID,String user) {
+       this.stickerID=stickerID;
+       this.user=user;
+       time=new Date().getTime();
+    }
+
+    public Message() {
+
+    }
+
+    public String getStickerID() {
+        return stickerID;
+    }
+
+    public void setStickerID(String stickerID) {
+        this.stickerID=stickerID;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user=user;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+}
